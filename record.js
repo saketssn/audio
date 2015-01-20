eb90afd03a717569630dc71106194b70b3f876b8
saketssn <saketssn@gmail.com>
2015-01-20 13:41:47 -0500

    Updated Audio Logs

---
+++ record.js
@@ -0,0 +1,11 @@
+Function record(){
+    if (!navigator.getUserMedia)
+         navigator.getUserMedia = navigator.getUserMedia ||                     navigator.webkitGetUserMedia ||
+            navigator.mozGetUserMedia || 
+            navigator.msGetUserMedia;
+ 
+if (navigator.getUserMedia){
+    navigator.getUserMedia({audio:true}, success, function(e) {
+    alert('Error capturing audio.');
+    });
+} else alert('getUserMedia not supported in this browser.');
